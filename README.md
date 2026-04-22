3b44b2d64eed31cd8b6c706a412ea0d97f73f9bb
Dopernieux <m.szczygiel2327@gmail.com>
2026-04-22 21:21:36 +0200

    init: 1d burgers equation solver

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# computational-fluid-dynamics
+# Computational Fluid Dynamics - From Scratch
+
+Implementation of basic CFD equations in 1D from scratch.
+
+Implementacje równań CFD w 1D napisane od podstaw.
+
+## Models
+
+- Linear Convection
+- Nonlinear Convection
+- Burgers' Equation
+
+## Results
+
+![Burgers](results/1d-burgers-equation.png)
+
+## Run
+
+pip install -r requirements.txt
+python src/1d-burgers-equation.py
---
+++ src/1d-burgers-equation.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 16:32:22 2026
+
+@author: szczv
+
+Twierdzenie Burgersa, rozwiązanie numeryczne (u) i analityczne (un)
+"""
+
+import numpy
+from matplotlib import pyplot
+
+nx = 50
+nt = 50
+dt = 0.01
+vis = 0.1
+dx = 2 * numpy.pi / (nx-1)
+
+u = numpy.ones(nx)
+ua = numpy.ones(nx)
+ip1 = numpy.ones(nx, dtype=int)
+im1 = numpy.ones(nx, dtype=int)
+x = numpy.copy(u)
+
+
+for i in range(nx):
+    ip1[i] = i+1
+    im1[i] = i-1
+    x[i] = (i-1)*dx
+    
+ip1[nx-1] = 0
+im1[0] = nx-1
+
+for i in range(nx):
+    phi = numpy.exp(-(x[i]**2)/(4*vis)) + numpy.exp((-(x[i]-2*(numpy.pi))**2/(4*vis)))
+    
+    dphi = (-(1/2)*(x[i]/vis))*numpy.exp(-(x[i]**2)/(4*vis))+((-(x[i]-2*(numpy.pi)))/(2*vis))*numpy.exp((-(x[i]-2*(numpy.pi))**2/(4*vis)))
+    
+    u[i] = (-2)*vis*(dphi/phi)+4
+
+
+for it in range(nt):
+    t = it*dt
+    
+    un = numpy.copy(u)
+    
+    for i in range(nx):
+        u[i] = un[i]-un[i]*(dt/dx)*( un[i] - un[im1[i]]) + vis * (dt/(dx**2)) * (un[ip1[i]]-2*un[i]+un[im1[i]])
+        
+    for i in range(nx):
+        phi = numpy.exp( -(x[i]-4*t)**2 / (4*vis*(t+1))) + numpy.exp(-(x[i]-4*t-2*numpy.pi)**2/(4*vis*(t+1)))
+        
+        dphi = ((-1/(2*vis*(t+1)))*(x[i]-4*t) * numpy.exp(-(x[i]-4*t)**2 / (4*vis*(t+1))) + ((-1/(2*vis*(t+1)))*(x[i]-4*t-2*numpy.pi)*numpy.exp(-(x[i]-4*t-2*numpy.pi)**2/(4*vis*(t+1)))))
+        
+        ua[i] = -2*vis*(dphi/phi)+4
+
+pyplot.plot(numpy.linspace(0, 2*(numpy.pi), nx), u, label='numeryczne')
+pyplot.plot(numpy.linspace(0, 2*(numpy.pi), nx), ua, label='analityczne')
+
+pyplot.text(0.02, 0.05,
+            r'$\frac{\partial u}{\partial t} + u\frac{\partial u}{\partial x} = \nu\frac{\partial^2 u}{\partial x^2}$',
+            transform=pyplot.gca().transAxes,
+            fontsize=13)
+
+pyplot.legend()
+pyplot.show()
+
+
+    
+    
